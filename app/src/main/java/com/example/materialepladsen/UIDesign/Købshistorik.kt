2c74811659b2155260d86a476fd33fc940a9b522
DenizIsikli <s215818@dtu.dk>
2022-10-24 16:24:52 +0200

    Added lazy column to the page

--- app/src/main/java/com/example/materialepladsen/UIDesign/Købshistorik.kt
+++ app/src/main/java/com/example/materialepladsen/UIDesign/Købshistorik.kt
@@ -2,11 +2,13 @@ package com.example.materialepladsen.UIDesign
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -32,46 +34,75 @@ fun Købshistorik (){
 
     ){
         //Lazycolum?
-        Column(
-            modifier = Modifier.fillMaxWidth()
 
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(800.dp)
         ){
+            item{
+                Logo2()
+            }
 
-            Logo2()
+            item {
+                Divider(
+                    modifier = Modifier.fillMaxWidth(),
+                    thickness = 1.dp,
+                    color = Color.Black
+                )
+            }
 
-            Divider(modifier = Modifier.fillMaxWidth(),
-                thickness = 1.dp,
-                color = Color.Black)
-            
-            KøbItem(materiale = R.string.Vare1,
-                vægt = R.string.Vægt1,
-                dato = R.string.Dato1,
-                pris =R.string.Pris1 ,
-                ordrenr = R.string.Ordrenummer1)
+            item {
+                KøbItem(
+                    materiale = R.string.Vare1,
+                    vægt = R.string.Vægt1,
+                    dato = R.string.Dato1,
+                    pris = R.string.Pris1,
+                    ordrenr = R.string.Ordrenummer1
+                )
+            }
 
-            KøbItem(materiale = R.string.Vare2,
-                vægt = R.string.Vægt2,
-                dato = R.string.Dato2,
-                pris =R.string.Pris2,
-                ordrenr = R.string.Ordrenummer2)
+            item {
+                KøbItem(
+                    materiale = R.string.Vare2,
+                    vægt = R.string.Vægt2,
+                    dato = R.string.Dato2,
+                    pris = R.string.Pris2,
+                    ordrenr = R.string.Ordrenummer2
+                )
+            }
 
-            KøbItem(materiale = R.string.Vare3,
-                vægt = R.string.Vægt3,
-                dato = R.string.Dato3,
-                pris =R.string.Pris3,
-                ordrenr = R.string.Ordrenummer3)
+            item {
+                KøbItem(
+                    materiale = R.string.Vare3,
+                    vægt = R.string.Vægt3,
+                    dato = R.string.Dato3,
+                    pris = R.string.Pris3,
+                    ordrenr = R.string.Ordrenummer3
+                )
+            }
 
-            KøbItem(materiale = R.string.Vare4,
-                vægt = R.string.Vægt4,
-                dato = R.string.Dato4,
-                pris =R.string.Pris4,
-                ordrenr = R.string.Ordrenummer4)
+            item {
+                KøbItem(
+                    materiale = R.string.Vare4,
+                    vægt = R.string.Vægt4,
+                    dato = R.string.Dato4,
+                    pris = R.string.Pris4,
+                    ordrenr = R.string.Ordrenummer4
+                )
+            }
 
-            KøbItem(materiale = R.string.Vare5,
-                vægt = R.string.Vægt5,
-                dato = R.string.Dato5,
-                pris =R.string.Pris5,
-                ordrenr = R.string.Ordrenummer5)
+            item {
+                KøbItem(
+                    materiale = R.string.Vare5,
+                    vægt = R.string.Vægt5,
+                    dato = R.string.Dato5,
+                    pris = R.string.Pris5,
+                    ordrenr = R.string.Ordrenummer5
+                )
+            }
         }
     }
 }

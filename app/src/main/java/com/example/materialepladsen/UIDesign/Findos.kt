79593e462b47a6017a3af3aedfbb3575eb7c2964
DenizIsikli <s215818@dtu.dk>
2022-10-27 15:47:34 +0200

    Minor changes

--- app/src/main/java/com/example/materialepladsen/UIDesign/Findos.kt
+++ app/src/main/java/com/example/materialepladsen/UIDesign/Findos.kt
@@ -1,46 +1,20 @@
 package com.example.materialepladsen.UIDesign
 
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForwardIos
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.blue
 import com.example.materialepladsen.R
 import com.example.materialepladsen.ui.theme.*
 
@@ -73,7 +47,6 @@ fun Findos () {
             )
 
             Spacer(modifier = Modifier.height(15.dp))
-/////////////////////////////////////////////////////////////////////
 
             Row(modifier=Modifier.padding(start=0.dp)) {
                 Spacer(modifier = Modifier.width(1.dp))
@@ -82,8 +55,9 @@ fun Findos () {
                 )
                 Spacer(modifier = Modifier.width(1.dp))
             }
-/////////////////////////////////////////////////////////////////////
+
             Spacer(modifier = Modifier.height(15.dp))
+
             Row(modifier=Modifier.padding(start=0.dp)) {
                 Spacer(modifier = Modifier.width(1.dp))
                 Rectanglefindos(
@@ -116,10 +90,6 @@ fun Findos () {
                 )
                 Spacer(modifier = Modifier.width(1.dp))
             }
-
-
-
-
         }
     }
 }
@@ -166,7 +136,6 @@ fun Initialize_Map(
     }
 }
 
-
 @Composable
 fun Rectanglefindos(
     @DrawableRes billede: Int,
@@ -187,11 +156,9 @@ fun Rectanglefindos(
     }
 }
 
-
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview9() {
+fun DefaultPreview14() {
     MaterialepladsenTheme {
         Findos()
     }}

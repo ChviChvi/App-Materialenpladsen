2681de1303b6db657bb18676886ec24fd35f9f13
DenizIsikli <s215818@dtu.dk>
2022-11-08 23:37:11 +0100

    Added scrollable row functions for the list of materials

--- app/src/main/java/com/example/materialepladsen/UIDesign/Materialer.kt
+++ app/src/main/java/com/example/materialepladsen/UIDesign/Materialer.kt
@@ -1,30 +1,58 @@
 package com.example.materialepladsen.ui.theme
 
+import androidx.compose.ui.tooling.preview.Preview
+import android.os.Bundle
 import android.annotation.SuppressLint
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
 import com.example.materialepladsen.R
-import com.example.materialepladsen.ui.theme.MaterialepladsenTheme
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.Dp
+import com.example.materialepladsen.viewmodel.MaterialList
+import com.example.materialepladsen.viewmodel.Material_list
+
+
+
+class Materials : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var selectedTabIndex by remember { mutableStateOf(0) }
+            val tabs = MaterialList.valueOf(Material_list.toString())
+
+            CustomScrollableTabRow(
+                tabs = tabs,
+                selectedTabIndex = selectedTabIndex
+            ) { tabIndex ->
+                selectedTabIndex = tabIndex
+            }
+        }
+    }
+}
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -56,21 +84,12 @@ fun Materialer () {
             //Første række
             //Måske laves til LazyRow?
             Row(modifier=Modifier.padding(start=28.dp)) {
-                Materialeitem(
-                    id_et = R.string.askvægt,
-                    id_to = R.string.askalder,
-                    id_tre = R.string.askpris,
-                    billede = R.drawable.woodpile_6364896_960_720_2,
-                )
+
+                //CustomScrollableTabRow
 
                 Spacer(modifier = Modifier.width(50.dp))
 
-                Materialeitem(
-                    id_et = R.string.bøgvægt,
-                    id_to = R.string.bøgalder,
-                    id_tre = R.string.bøgpris,
-                    billede = R.drawable.woodpile_6364896_960_720_3,
-                )
+                //CustomScrollableTabRow
                 
                 Spacer(modifier = Modifier.width(13.dp))
 
@@ -100,21 +119,12 @@ fun Materialer () {
             //Anden række
             //Måske laves til LazyRow?
             Row(modifier=Modifier.padding(start=28.dp)) {
-                Materialeitem(
-                    id_et = R.string.Sølvpladetykkelse,
-                    id_to = R.string.sølvpladeinfo,
-                    id_tre = R.string.Sølvpladepris,
-                    billede = R.drawable.s_lvplade
-                )
+
+                //CustomScrollableTabRow
 
                 Spacer(modifier = Modifier.width(50.dp))
 
-                Materialeitem(
-                    id_et = R.string.ASølvplade,
-                    id_to = R.string.AInfo,
-                    id_tre = R.string.Apris,
-                    billede = R.drawable.as_lvplade,
-                )
+                //CustomScrollableTabRow
 
                 Spacer(modifier = Modifier.width(13.dp))
 
@@ -145,21 +155,11 @@ fun Materialer () {
             //Tredje række
             //Måske laves til LazyRow?
             Row(modifier=Modifier.padding(start=28.dp)) {
-                Materialeitem(
-                    id_et = R.string.Hvide,
-                    id_to = R.string.Hvideinfo,
-                    id_tre = R.string.HvidePris,
-                    billede = R.drawable.hvide,
-                )
+                //CustomScrollableTabRow
 
                 Spacer(modifier = Modifier.width(50.dp))
 
-                Materialeitem(
-                    id_et = R.string.Røde,
-                    id_to = R.string.RødeInfo,
-                    id_tre = R.string.RødePris,
-                    billede = R.drawable.r_de,
-                )
+                //CustomScrollableTabRow
 
                 Spacer(modifier = Modifier.width(13.dp))
 
@@ -202,46 +202,115 @@ fun Logo2(){
 }
 
 @Composable
-fun Materialeitem(
-    //Strings for product - Product/
-    @StringRes id_et: Int,
-    @StringRes id_to: Int,
-    @StringRes id_tre: Int,
-    @DrawableRes billede: Int,
+fun CustomScrollableTabRow(
+    tabs: List<String>,
+    selectedTabIndex: Int,
+    onTabClick: (Int) -> Unit
 ) {
-
-    Column(modifier = Modifier.width(140.dp)) {
-        Image(
-            modifier = Modifier
-                .height(81.dp)
-                .width(98.dp)
-                .align(CenterHorizontally),
-            painter = painterResource(id = billede),
-            contentDescription = null
-        )
-        Text(text = stringResource(id = id_et),
-            style=MaterialTheme.typography.subtitle1,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            lineHeight = 14.sp
-        )
-
-        Text(text = stringResource(id = id_to),
-            style=MaterialTheme.typography.subtitle2,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            lineHeight = 14.sp
-        )
-
-        Text(text ="DKK "+ stringResource(id = id_tre),
-            style=MaterialTheme.typography.h3,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(),
-            lineHeight = 14.sp,
-            color = MaterialTheme.colors.secondaryVariant
-        )
+    val density = LocalDensity.current
+    val tabWidths = remember {
+        val tabWidthStateList = mutableStateListOf<Dp>()
+        repeat(tabs.size) {
+            tabWidthStateList.add(0.dp)
+        }
+        tabWidthStateList
+    }
+    ScrollableTabRow(
+        selectedTabIndex = selectedTabIndex,
+        contentColor = Color.White,
+        edgePadding = 0.dp,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.customTabIndicatorOffset(
+                    currentTabPosition = tabPositions[selectedTabIndex],
+                    tabWidth = tabWidths[selectedTabIndex]
+                )
+            )
+        }
+    ) {
+        tabs.forEachIndexed { tabIndex, tab ->
+            Tab(
+                selected = selectedTabIndex == tabIndex,
+                onClick = { onTabClick(tabIndex) },
+                text = {
+                    Text(
+                        text = tab,
+                        onTextLayout = { textLayoutResult ->
+                            tabWidths[tabIndex] =
+                                with(density) { textLayoutResult.size.width.toDp() }
+                        }
+                    )
+                }
+            )
+        }
     }
 }
+
+@Composable
+fun Modifier.customTabIndicatorOffset(
+    currentTabPosition: TabPosition,
+    tabWidth: Dp
+): Modifier = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "customTabIndicatorOffset"
+        value = currentTabPosition
+    }
+) {
+    val currentTabWidth by animateDpAsState(
+        targetValue = tabWidth,
+        animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing)
+    )
+    val indicatorOffset by animateDpAsState(
+        targetValue = ((currentTabPosition.left + currentTabPosition.right - tabWidth) / 2),
+        animationSpec = tween(durationMillis = 250, easing = FastOutSlowInEasing)
+    )
+    fillMaxWidth()
+        .wrapContentSize(Alignment.BottomStart)
+        .offset(x = indicatorOffset)
+        .width(currentTabWidth)
+}
+
+//@Composable
+//fun Materialeitem(
+//    //Strings for product - Product/
+//    @StringRes id_et: Int,
+//    @StringRes id_to: Int,
+//    @StringRes id_tre: Int,
+//    @DrawableRes billede: Int,
+//) {
+//
+//    Column(modifier = Modifier.width(140.dp)) {
+//        Image(
+//            modifier = Modifier
+//                .height(81.dp)
+//                .width(98.dp)
+//                .align(CenterHorizontally),
+//            painter = painterResource(id = billede),
+//            contentDescription = null
+//        )
+//        Text(text = stringResource(id = id_et),
+//            style=MaterialTheme.typography.subtitle1,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier.fillMaxWidth(),
+//            lineHeight = 14.sp
+//        )
+//
+//        Text(text = stringResource(id = id_to),
+//            style=MaterialTheme.typography.subtitle2,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier.fillMaxWidth(),
+//            lineHeight = 14.sp
+//        )
+//
+//        Text(text ="DKK "+ stringResource(id = id_tre),
+//            style=MaterialTheme.typography.h3,
+//            textAlign = TextAlign.Center,
+//            modifier = Modifier.fillMaxWidth(),
+//            lineHeight = 14.sp,
+//            color = MaterialTheme.colors.secondaryVariant
+//        )
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable

c6f6a8625da6d4ada473a59938d99b9bc6297fcd
Villads <villadsandersen1@gmail.com>
2022-11-09 12:28:40 +0100

    List of orders. funtion to add order to list of orders

--- app/src/main/java/com/example/materialepladsen/viewmodel/KøbHistorikUIState.kt
+++ app/src/main/java/com/example/materialepladsen/viewmodel/KøbHistorikUIState.kt
@@ -1,11 +1,14 @@
 package com.example.materialepladsen.viewmodel
 
-import com.example.materialepladsen.R
+import androidx.navigation.NavController
+
 
 data class KøbHistorikUIState (
     val customerID: Long,
     val orders: List<Order>,
 )
+
+val Købshistorikliste = mutableListOf<Order>()
 
 
 data class Order (
@@ -15,3 +18,16 @@ data class Order (
     val pris: Double,
     val ordrenr: Int,
 )
+
+fun addToBuyHistory(
+    materiale:String,
+    vægt: Float,
+    dato: String,
+    pris: Double,
+    ordrenr: Int,
+    navController: NavController
+){
+    Købshistorikliste.add(Order(materiale,vægt,dato,pris,ordrenr))
+    navController.navigate("Købshistorik")
+}
+

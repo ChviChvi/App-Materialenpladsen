592977f53f7b42865cdba54d642d84d63c54ed0a
Villads <villadsandersen1@gmail.com>
2023-01-17 16:37:32 +0100

    logo deleted

--- app/src/main/java/com/example/materialepladsen/UIDesign/Omos.kt
+++ app/src/main/java/com/example/materialepladsen/UIDesign/Omos.kt
@@ -1,35 +1,21 @@
 package com.example.materialepladsen.ui.theme
 
 import android.annotation.SuppressLint
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-
-
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-
-import androidx.compose.ui.text.style.TextAlign
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.materialepladsen.R
-import com.example.materialepladsen.ui.theme.*
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -42,9 +28,7 @@ fun Omos(navController: NavController) {
             .height(800.dp)
 
     ){
-        item {
-            Logo2()
-        }
+
         item {
             Divider(color = Color.Black, thickness = 1.2.dp)
         }
